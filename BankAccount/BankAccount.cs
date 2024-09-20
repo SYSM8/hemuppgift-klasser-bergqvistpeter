be542c826726f823a1e42f0cfdebd5b9ab443815
bergqvistpeter <peter.bergqvist@edu.newton.se>
2024-09-20 09:27:06 +0200

    Corrected an string to a double. added methods

--- BankAccount/BankAccount.cs
+++ BankAccount/BankAccount.cs
@@ -11,19 +11,42 @@ namespace BankAccount
         //Lägg till Egenskaper (fields)
         public string AccountNumber { get; set; }
         public string AccountHolder {  get; set; }
-        public string Balance { get; set; }
+        public double Balance { get; set; }
 
-        public BankAccount(string konto, string kontoinnehavare, string saldo) 
+        //Lägg till Konstruktor
+        public BankAccount(string konto, string kontoinnehavare, double saldo) 
         { 
             AccountNumber = konto;
             AccountHolder = kontoinnehavare;
             Balance = saldo; 
         }
-
-
-        //Lägg till Konstruktor
-
         //Lägg till Metoder
+
+        public double Deposit(double addBalance) 
+        {
+        return Balance + addBalance;
+        }
+
+        public double Withdraw(double withdrawBalance) 
+        {
+
+
+            if (Balance - withdrawBalance >= 0)
+            {
+                return Balance - withdrawBalance;
+            }
+            else
+            {
+                Console.WriteLine($"Du försöker ta ut {withdrawBalance} men har bara {Balance} på ditt konto");
+            }  
+            return Balance;
+        }
+
+        public void DisplayBalance() 
+        
+        { 
+            Console.WriteLine($"Ditt saldo är: {Balance}");
+        }
 
         //Lycka till! :)
     }
--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -4,12 +4,15 @@
     {
         static void Main(string[] args)
         {
-            //Instansiera bankAccount klassen och testa den
-            Employee employee = new Employee("Peter", "Bergqvist");
-            employee.Salary = 3000;
+            //Employee employee = new Employee("Peter", "Bergqvist");
+            //employee.Salary = 3000;
 
-            employee.GetAnnualSalary();
-            employee.GiveRaise(10);
+            //employee.GetAnnualSalary();
+            //employee.GiveRaise(10);
+            //Instansiera bankAccount klassen och testa den
+
+            BankAccount B1 = new BankAccount("123456789", "Peter", 1000);
+
 
         }
     }
